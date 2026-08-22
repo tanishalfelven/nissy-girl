e41ca965c2840d35fe8d691fd1d80fe2072c3765
Tanis Newlun <6371353+tanishalfelven@users.noreply.github.com>
2026-08-22 01:52:01 -0600

    add zone generation difficulty scaling

--- src/game/jumper/generation.entity/generation.consts.js
+++ src/game/jumper/generation.entity/generation.consts.js
@@ -1,4 +1,5 @@
 export const ZONE_HEIGHT = 150;
 export const START_ZONE = 0;
 export const ZONE_1 = 1;
-export const MIN_PLATFORM_WIDTH = 15;
+export const MAX_PLATFORM_WIDTH = 30;
+export const MIN_PLATFORM_WIDTH = 10;
--- src/game/jumper/generation.entity/generator.component.js
+++ src/game/jumper/generation.entity/generator.component.js
@@ -1,4 +1,4 @@
-import { randRange, randBool, lerp } from "$util/math.js";
+import { randRange, roundDigit, lerp, randBool } from "$util/math.js";
 
 import { CANVAS_WIDTH } from "$nissy-girl/screens/screen.consts.js";
 
@@ -7,7 +7,15 @@ import {
 	START_ZONE,
 	ZONE_1,
 	MIN_PLATFORM_WIDTH,
+	MAX_PLATFORM_WIDTH,
 } from "./generation.consts.js";
+
+const MAX_HOP_ODDS = 0.85;
+
+const odds = (odds) => Math.random() <= odds;
+const getRand = (min, max) => roundDigit(randRange(min, max), 4);
+
+const isHop = (difficulty = 0) => odds(MAX_HOP_ODDS - difficulty * MAX_HOP_ODDS);
 
 const forceInBounds = (platform, forceWidth = CANVAS_WIDTH) => {
 	if((platform.x + platform.width) < 0) {
@@ -44,39 +52,75 @@ export const DEV_MAP = [
 	platform(55, 25),
 ];
 
-const generateShortPlatform = (j, fromPlatform, zone = ZONE_1, isHop = false) => {
+const generateX = ({
+	fromPlatform,
+	maxX,
+	difficulty,
+}) => {
+	const center = fromPlatform.x + (fromPlatform.width / 2);
+	const maxDistance = (fromPlatform.width / 2) + maxX;
+	const minDistance = lerp(difficulty, 0, maxDistance * 0.75);
+
+	const distance = getRand(minDistance, maxDistance);
+	const direction = randBool() ? -1 : 1;
+
+	return center + distance * direction;
+};
+
+const generateShortPlatform = ({
+	capabilities : j,
+	fromPlatform,
+	zone = ZONE_1,
+	isHop = false,
+	difficulty,
+}) => {
 	const min = isHop ? j.hopMin : j.blastMin;
 	const max = isHop ? j.hopMax : j.blastMax;
 
-	console.log({ isHop, min, max, j });
+	// min distance slowly raises with difficulty
+	const minJump = lerp(difficulty, min.vert, max.vert);
 
-	const jumpHeight = randRange(min.vert, max.vert);
+	const jumpHeight = getRand(minJump, max.vert);
 	const maxX = lerp(
-		jumpHeight / (max.vert - min.vert),
+		(jumpHeight - min.vert) / (max.vert - min.vert),
 		min.horz,
 		max.horz,
 	);
 
 	const y = fromPlatform.y - jumpHeight;
-	const x = randRange(fromPlatform.x - maxX, fromPlatform.x + fromPlatform.width + maxX);
+	const x = generateX({ fromPlatform, maxX, difficulty });
 
-	return platform(x, y, MIN_PLATFORM_WIDTH, zone);
+	// platforms steadily get smaller as you go up
+	const minPlatformSize = lerp(
+		difficulty,
+		MAX_PLATFORM_WIDTH,
+		MIN_PLATFORM_WIDTH + (MAX_PLATFORM_WIDTH - MIN_PLATFORM_WIDTH) * (1 - difficulty),
+	);
+	const platformSize = getRand(MIN_PLATFORM_WIDTH, minPlatformSize);
+
+	return platform(x, y, platformSize, zone);
 };
 
-const generateZone = (capabilities, entryPlatform, zone = ZONE_1) => {
+const generateZone = ({
+	capabilities,
+	fromPlatform,
+	zone = ZONE_1,
+	difficulty = 0,
+}) => {
 	const platforms = [];
 
-	const targetY = entryPlatform.y - ZONE_HEIGHT;
-	let mostRecentPlatform = entryPlatform;
-	let highestPlatformY = entryPlatform.y;
+	const targetY = fromPlatform.y - ZONE_HEIGHT;
+	let mostRecentPlatform = fromPlatform;
+	let highestPlatformY = fromPlatform.y;
 
 	do{
-		const nextPlaform = generateShortPlatform(
+		const nextPlaform = generateShortPlatform({
 			capabilities,
-			mostRecentPlatform,
+			fromPlatform : mostRecentPlatform,
 			zone,
-			randBool(),
-		);
+			isHop : isHop(),
+			difficulty,
+		});
 
 		platforms.push(nextPlaform);
 
@@ -90,13 +134,16 @@ const generateZone = (capabilities, entryPlatform, zone = ZONE_1) => {
 const generate = (capabilities, startPlatform = START_PLATFORM) => {
 	const platforms = [ startPlatform ];
 
-	for(let zone = ZONE_1; zone <= 8; zone++) {
+	const NUM_ZONES = 8;
+
+	for(let zone = ZONE_1; zone <= NUM_ZONES; zone++) {
 		platforms.push(
-			...generateZone(
+			...generateZone({
 				capabilities,
-				platforms[platforms.length - 1],
+				fromPlatform : platforms[platforms.length - 1],
 				zone,
-			),
+				difficulty : zone / NUM_ZONES,
+			}),
 		);
 	}
 
