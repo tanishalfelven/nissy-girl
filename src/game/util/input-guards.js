51cb6db0741b6ccf23bbfb0caf1c6428883fdcf1
Tanis Newlun <6371353+tanishalfelven@users.noreply.github.com>
2026-08-27 17:31:03 -0600

    release helper

--- src/game/util/input-guards.js
+++ src/game/util/input-guards.js
@@ -1,3 +1,4 @@
-import { TRIGGERED } from "$game/shared/input.consts.js";
+import { TRIGGERED, RELEASED } from "$game/shared/input.consts.js";
 
 export const inputTriggered = ({ event }) => event.state === TRIGGERED;
+export const inputReleased = ({ event }) => event.state === RELEASED;
