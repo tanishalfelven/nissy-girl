b53f680e117232d932abcc84d1b9f587a7522115
Tanis Newlun <6371353+tanishalfelven@users.noreply.github.com>
2026-08-27 00:18:25 -0600

    paint - only render swirl on create

--- src/game/paint/ui/swirl.entity.js
+++ src/game/paint/ui/swirl.entity.js
@@ -15,6 +15,32 @@ export const createSwirl = ({
 
 	swirl.position.set(worldBounds.width / 2, worldBounds.height / 2);
 
+	for(let arm = 0; arm < ARMS; arm++) {
+		const armOffset = (Math.PI * 2 * arm) / ARMS;
+
+		for(let i = 0; i < POINTS_PER_ARM; i++) {
+			const t = i / (POINTS_PER_ARM - 1);
+
+			const radius = t * MAX_RADIUS;
+			const angle = armOffset + t * Math.PI;
+
+			const x = Math.cos(angle) * radius;
+			const y = Math.sin(angle) * radius;
+
+			swirl
+				.rect(
+					x,
+					y,
+					4 * t,
+					4 * t,
+				)
+				.fill({
+					color : arm % 2 === 0 ? 0x839cf6 : 0x9baff8,
+					alpha : i / POINTS_PER_ARM,
+				});
+		}
+	}
+
 	return createEntity({
 		id : "swirl",
 		components : {
@@ -25,34 +51,6 @@ export const createSwirl = ({
 
 				update(dt) {
 					swirl.rotation += 0.002 * dt;
-
-					swirl.clear();
-
-					for(let arm = 0; arm < ARMS; arm++) {
-						const armOffset = (Math.PI * 2 * arm) / ARMS;
-
-						for(let i = 0; i < POINTS_PER_ARM; i++) {
-							const t = i / (POINTS_PER_ARM - 1);
-
-							const radius = t * MAX_RADIUS;
-							const angle = armOffset + t * Math.PI;
-
-							const x = Math.cos(angle) * radius;
-							const y = Math.sin(angle) * radius;
-
-							swirl
-								.rect(
-									x,
-									y,
-									4 * t,
-									4 * t,
-								)
-								.fill({
-									color : arm % 2 === 0 ? 0x839cf6 : 0x9baff8,
-									alpha : i / POINTS_PER_ARM,
-								});
-						}
-					}
 				},
 
 				getRenderable() {
