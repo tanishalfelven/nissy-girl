e84026b99b824493c653718195daccb6a225fb0b
Tanis Newlun <6371353+tanishalfelven@users.noreply.github.com>
2026-09-02 22:06:13 -0600

    prompts inputs pass and initial test
    !!!

--- src/nissy-girl/nissy-girl.svelte
+++ src/nissy-girl/nissy-girl.svelte
@@ -7,6 +7,8 @@ import FaceControls from "./controls/front-controls.svelte";
 import PowerSwitch from "./controls/power-switch/power-switch.svelte";
 import VolumeWheel from "./controls/volume/volume-wheel.svelte";
 import Screen from "./screens/screen.svelte";
+// TODO sizing pass :)
+// import Prompts from "./prompts/prompts.svelte";
 
 import { nissyGirl } from "./nissy-girl.viewmodel.svelte.js";
 import { camera, zoom } from "./camera.viewmodel.svelte.js";
@@ -21,6 +23,9 @@ const displayZoom = $derived(roundHundredths(zoom.progress * 10));
 
 let nissyGirlWidth = $state(false);
 </script>
+
+<!-- SOON! -->
+<!-- <Prompts /> -->
 
 <div
 	class={css.camera}
---
+++ src/nissy-girl/prompts/prompts.svelte
@@ -0,0 +1,9 @@
+<script>
+import css from "./prompts.mcss";
+</script>
+
+<div class={css.prompts}>
+	<div class={css.rotate}>
+		view cartridges
+	</div>
+</div>
