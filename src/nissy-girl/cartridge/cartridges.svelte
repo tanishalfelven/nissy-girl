396af9474eee30a4735469cf79121843529ac08f
Tanis Newlun <6371353+tanishalfelven@users.noreply.github.com>
2026-09-05 12:06:37 -0600

    more cartridge restructuring
    bring back in/out event
    
    fix some logic for which cartridges are display on in/out

--- src/nissy-girl/cartridge/cartridges.svelte
+++ src/nissy-girl/cartridge/cartridges.svelte
@@ -8,51 +8,61 @@ import { gameOrder } from "$game/games.js";
 
 import cartridgeCss from "./cartridge.mcss";
 import cartridgesCss from "./cartridges.mcss";
+
+$effect(() => {
+	console.log(cartridges.cartridgeIndex);
+});
 </script>
 
-{#if cartridges.isVisible}
-	{#if cartridges.cartridgeIndex > 0}
-		<div class={cartridgesCss.cartridges} data-prev="true">
-			{#each { length : cartridges.cartridgeIndex } as _, cartridgeIndex}
-				{@const isOnFinal = cartridges.cartridgeIndex === gameOrder.length - 1}
-				{@const inEnding = cartridgeX.progress >= 0.5}
-				{@const x = isOnFinal && inEnding ? 0.3 : -0.2 + cartridgeX.progress}
-				{@const backProgress = (cartridgeX.progress - 0.5) / 0.5}
-				{@const y = isOnFinal && inEnding ? lerp(1 - backProgress, -0.5, 1) : 1}
+<div class={cartridgesCss.cartridges} data-visible={cartridges.isVisible}>
+	{#if cartridges.cartridgeIndex > 0 && cartridges.cartridgeIndex < gameOrder.length}
+		{#each { length : cartridges.cartridgeIndex } as _, cartridgeIndex}
+			{@const isOnFinal = cartridges.cartridgeIndex === gameOrder.length - 1}
+			{@const inEnding = cartridgeX.progress >= 0.5}
+			{@const x = isOnFinal && inEnding ? 0.2 : -0.3 + cartridgeX.progress}
+			{@const backProgress = (cartridgeX.progress - 0.5) / 0.5}
+			{@const y = isOnFinal && inEnding ? lerp(1 - backProgress, -0.5, 1) : 1}
+			{@const cartridge = gameOrder[cartridgeIndex]}
 
+			{#if cartridge}
 				<div
 					style="--cartridgex: {x};
 					--cartridgey: {y * 1};
 					--rotatey: 0deg;"
 					class={cartridgeCss.cartridge}
 					data-background="true"
+					data-cartridge={cartridge}
+					data-prev="true"
 				>
 					<Cartridge cartridge={gameOrder[cartridgeIndex]} />
 				</div>
-			{/each}
-		</div>
+			{/if}
+		{/each}
 	{/if}
 
-	{#if cartridges.cartridgeIndex < gameOrder.length - 1}
-		<div class={cartridgesCss.cartridges} data-next="true">
-			{#each { length : gameOrder.length - 1 - cartridges.cartridgeIndex } as _, offsetIndex}
-				{@const cartridgeIndex = 1 + offsetIndex + cartridges.cartridgeIndex}
-				{@const isEntering = cartridges.cartridgeIndex === 0 && cartridgeX.progress <= 0.5}
-				{@const startProgress = cartridgeX.progress / 0.5}
-				{@const backProgress = (cartridgeX.progress - 0.5) / 0.5}
-				{@const x = isEntering ? 0 : backProgress}
-				{@const y = isEntering ? lerp(startProgress, -0.5, 1) : 1}
+	{#if cartridges.cartridgeIndex < gameOrder.length}
+		{#each { length : gameOrder.length - 1 - cartridges.cartridgeIndex } as _, offsetIndex}
+			{@const cartridgeIndex = 1 + offsetIndex + cartridges.cartridgeIndex}
+			{@const isEntering = cartridges.cartridgeIndex === 0 && cartridgeX.progress <= 0.5}
+			{@const startProgress = cartridgeX.progress / 0.5}
+			{@const backProgress = (cartridgeX.progress - 0.5) / 0.5}
+			{@const x = isEntering ? 0 : backProgress}
+			{@const y = isEntering ? lerp(startProgress, -0.5, 1) : 1}
+			{@const cartridge = gameOrder[cartridgeIndex]}
 
+			{#if cartridge}
 				<div
-					style="--cartridgex: {0.7 + x * 0.4};
+					style="--cartridgex: {0.7 + x * 0.6};
 					--cartridgey: {y * 1};
 					--rotatey: 0deg;"
 					class={cartridgeCss.cartridge}
 					data-background="true"
+					data-cartridge={cartridge}
+					data-next="true"
 				>
-					<Cartridge cartridge={gameOrder[cartridgeIndex]} />
+					<Cartridge {cartridge} />
 				</div>
-			{/each}
-		</div>
+			{/if}
+		{/each}
 	{/if}
-{/if}
+</div>
